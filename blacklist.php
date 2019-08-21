5e92169ae2716cd70c112f2d6b61c09fcf01c11e
aheartforspinach <nsr97@gmx.de>
2019-08-21 15:43:40 +0200

    Add Files

---
+++ blacklist.php
@@ -0,0 +1,175 @@
+<?php
+define("IN_MYBB", 1);
+// define('THIS_SCRIPT', 'blacklist.php');
+// require_once "./global.php";
+require("global.php");  
+
+global $db, $templates, $mybb;
+$email = $mybb->user['email'];
+$uid = $mybb->user['uid'];
+
+if(!isset($mybb->settings['blacklist_applicant'])) {
+    die("Die Blacklist ist zur Zeit nicht aktiviert.");
+}
+
+$username = "";
+$thisMonth = date("m.Y",time());
+$day = date("d", time());
+$blacklistUser = array();
+$blacklistUserAnnulled = array();
+$blacklistAll = array();
+$blacklistHelp = array();
+
+if ($mybb->settings['blacklist_guest'] == "0" && $mybb->user['uid'] == 0){
+    error_no_permission();
+}
+
+//Einstellungen holen
+if($mybb->settings['blacklist_ice'] == "-1"){
+    $fidIceDB = "";
+}else{
+    $fidIceDB = "fid" . intval($mybb->settings['blacklist_ice']) . ",";
+    $fidIce = intval($mybb->settings['blacklist_ice']);
+}
+$fidPlayer = intval($mybb->settings['blacklist_player']);
+$inplay = intval($mybb->settings['blacklist_inplay']);
+$archiv = intval($mybb->settings['blacklist_archive']);
+
+$dayEcho = intval($mybb->settings['blacklist_echo']);
+if($mybb->settings['blacklist_applicant'] == "1"){
+    $applicant = -1;
+}else{
+    $applicant = 2;
+}
+
+if($mybb->settings['blacklist_showUser'] == "1"){
+    $showOtherUsers = false;
+}else{
+    $showOtherUsers = true;
+}
+
+$invisibleAccounts = "";
+$accounts = explode(", ", $db->escape_string($mybb->settings['blacklist_teamaccs']));
+foreach($accounts as $account){
+    if($account != -1){
+        $invisibleAccounts .= "XOR uid = ". $account ." ";
+    }
+}
+
+if($fidPlayer == -1 || $inplay == -1 || $archiv == -1){
+    die("Fülle bitte zuerst die Einstellungen im AdminCP aus. Es muss mind. die FID vom Spielernamenprofilfeld, die ID der Inplaykategorie und die ID der 
+    Archivkategorie angebenen werden.");
+}
+
+//User streichen
+if($_POST["action"] == "delete"){
+    $uid = $_POST["id"];
+    array_push($blacklistUserAnnulled, $uid);
+}
+
+//alle User bekommen
+$allUsers = $db->query("SELECT uid FROM ".TABLE_PREFIX."users");
+while ($allUser=$db->fetch_array($allUsers)){
+    array_push($blacklistAll, $allUser['uid']);
+}
+
+//alle auf "nicht auf der Blacklist" setzen (nur am 1.)
+if(date("j", time()) == 1){
+    foreach($blacklistAll as $user){
+        $db->query("UPDATE ".TABLE_PREFIX."users SET isOnBlacklist = 0 WHERE uid = ". $user ."");
+    }
+}
+
+//Kontrolle, ob Post in diesem Monat oder letzten Monat => nicht Blacklist
+$lastIPPosts = $db->query("SELECT uid, dateline
+FROM ".TABLE_PREFIX."posts p JOIN ".TABLE_PREFIX."forums f ON f.fid = p.fid
+WHERE f.parentlist LIKE '" . $inplay . ",%' OR f.parentlist LIKE '%". $archiv ."%'");
+while ($lastIPPost=$db->fetch_array($lastIPPosts)){
+    if(!isOlderThanOneMonth($lastIPPost['dateline']) && !isInThisMonth($lastIPPost['dateline'])){
+        array_push($blacklistHelp, $lastIPPost['uid']);
+    }
+}
+
+//Differnez von allen User und denen, die nicht auf der BL sind
+$blacklistUser = array_diff($blacklistAll, $blacklistHelp);
+foreach($blacklistUser as $user){
+    $db->query("UPDATE ".TABLE_PREFIX."users SET isOnBlacklist = 1 WHERE uid = ". $user ."");
+}
+
+//gestrichene User finden (die, die gepostet haben und die, die vom Team gestrichen wurden)
+$lastIPPosts2 = $db->query("SELECT u.uid, dateline, isOnBlacklistAnnulled
+FROM ".TABLE_PREFIX."users u 
+JOIN ".TABLE_PREFIX."posts p ON p.uid = u.uid
+JOIN ".TABLE_PREFIX."forums f ON f.fid = p.fid
+WHERE isOnBlacklistAnnulled = 1 OR (isOnBlacklist = 1 AND (f.parentlist LIKE '" . $inplay . ",%' OR f.parentlist LIKE '%". $archiv ."%'))");
+while ($lastIPPost=$db->fetch_array($lastIPPosts2)){
+    if(isInThisMonth($lastIPPost['dateline']) || $lastIPPost['isOnBlacklistAnnulled'] == 1){
+        array_push($blacklistUserAnnulled, $lastIPPost['uid']);
+    }
+}
+
+foreach($blacklistUserAnnulled as $user){
+    $db->query("UPDATE ".TABLE_PREFIX."users SET isOnBlacklistAnnulled = 1 WHERE uid = ". $user ."");
+}
+
+// Charaktere, die auf der Blacklist, auf Eis oder abwesend sind
+$users = $db->query("SELECT username,usergroup,displaygroup,uid, " . $fidIceDB . " fid" . $fidPlayer . ",away,as_uid, email
+FROM ".TABLE_PREFIX."users u LEFT JOIN ".TABLE_PREFIX."userfields uf ON(u.uid=uf.ufid)
+WHERE NOT usergroup = " . $applicant . " " . $invisibleAccounts . "
+ORDER BY username");
+$black = "";
+$away = "";
+$onIce = "";
+$deleteButton = "";
+while($user=$db->fetch_array($users)) {
+    if(!$showOtherUsers && $user['email'] == $email || $showOtherUsers){
+        $username = build_profile_link(format_name($user['username'], $user['usergroup'], $user['displaygroup']), $user['uid']);
+        if($mybb->usergroup['canmodcp'] == 1 && $dayEcho >= date("j", time())){ //wenn User Admin oder Mod ist
+            $deleteButton = '<i class="fas fa-user-minus delete buttonBlack" title="' . $user['username'] . ' streichen?" id="' .  $user['uid']  . '"></i>';
+        }
+        if($user['fid' . $fidIce . ''] == "Ja"){
+            eval("\$onIce .= \"".$templates->get("blacklistUser")."\";");
+        }elseif($user['away'] == 1){
+            if($user['as_uid'] == 0){
+                $username = build_profile_link($user['fid' . $fidPlayer . ''], $user['uid']);
+                $deleteButton = "";
+                eval("\$away .= \"".$templates->get("blacklistUser")."\";");
+            }
+        }elseif(in_array($user['uid'], $blacklistUser) && !in_array($user['uid'], $blacklistUserAnnulled)){
+            eval("\$userBlack .= \"".$templates->get("blacklistUser")."\";");
+        }elseif(in_array($user['uid'], $blacklistUserAnnulled)){
+            eval("\$userBlack .= \"".$templates->get("blacklistUserAnnulled")."\";");
+        }
+    }
+}
+
+//Hilfsfunktionen
+function isOlderThanOneMonth($dateToTest){
+    $oneMonthAgo = strtotime("-1 month");
+    $firstOneMonthAgo = "01." . date("m.Y",$oneMonthAgo);
+
+    $UnixFirstOneMonthAgo = strtotime($firstOneMonthAgo);
+    if($dateToTest > $UnixFirstOneMonthAgo){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function isInThisMonth($dateToTest){
+    $thisMonth = date("m", time());
+    $monthDateToTest = date("m", $dateToTest);
+    if($monthDateToTest == $thisMonth){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+if($mybb->settings['blacklist_ice'] == "-1"){//auf eis ist deaktiviert
+    eval("\$page = \"".$templates->get("blacklist")."\";");
+}else{
+    eval("\$page = \"".$templates->get("blacklistIce")."\";");
+}
+output_page($page);
+?>
---
+++ inc/plugins/blacklist.php
@@ -0,0 +1,344 @@
+<?php
+// automatische Blacklist by aheartforspinach
+
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+
+function blacklist_info(){
+	return array(
+		"name"			=> "Blacklist",
+		"description"	=> "erstellt automatisch eine Liste von Charakteren, die auf der Blacklist stehen",
+		"author"		=> "aheartforspinach",
+		"authorsite"	=> "https://storming-gates.de/member.php?action=profile&uid=176",
+		"version"		=> "1.0",
+		"compatibility" => "18*"
+	);
+}
+
+function blacklist_install(){
+    global $db, $cache, $mybb;
+
+    $db->write_query("ALTER TABLE ".TABLE_PREFIX."users ADD isOnBlacklist INT(1) NOT NULL DEFAULT '0';");
+    $db->write_query("ALTER TABLE ".TABLE_PREFIX."users ADD isOnBlacklistAnnulled INT(1) NOT NULL DEFAULT '0';");
+    $db->write_query("ALTER TABLE ".TABLE_PREFIX."users ADD hasSeenBlacklist INT(1) NOT NULL DEFAULT '0';");
+
+    //Einstellungen 
+	$setting_group = array(
+      'name' => 'blacklist',
+      'title' => 'Blacklist',
+      'description' => 'Einstellungen für das Blacklist-Plugin',
+      'isdefault' => 0
+    );
+    $gid = $db->insert_query("settinggroups", $setting_group);
+
+    $setting_array = array(
+        'blacklist_guest' => array(
+            'title' => 'Sichtbarkeit',
+            'description' => 'Sollen Gäste die Blacklist sehen können?',
+            'optionscode' => 'yesno',
+            'value' => 1, // Default
+            'disporder' => 1
+        ),
+        'blacklist_applicant' => array(
+            'title' => 'Auflistung Bewerber',
+            'description' => 'Sollen Bewerber auch gelistet werden?',
+            'optionscode' => 'yesno',
+            'value' => 0, // Default
+            'disporder' => 2
+        ),
+        'blacklist_showUser' => array(
+            'title' => 'User verstecken',
+            'description' => 'Sollen User nur ihre eigenen Charaktere auf der BL sehen? Falls nein, sehen User alle Charaktere und nicht nur ihre',
+            'optionscode' => 'yesno',
+            'value' => 0, // Default
+            'disporder' => 3
+        ),
+        'blacklist_teamaccs' => array(
+            'title' => 'Teamaccount',
+            'description' => 'Gib hier mit Komma getrennt die UIDs von den Accounts an, die NICHT gelistet werden sollen. Falls alle gelistet werden sollen, gib -1 ein',
+            'optionscode' => 'text',
+            'value' => '998, 999', // Default
+            'disporder' => 4
+        ),
+        'blacklist_ice' => array(
+            'title' => 'Auf Eis Profilfeld',
+            'description' => 'Gib hier die ID von deinem Profilfeld ein, ob der Charakter auf Eis ist. -1 bedeutet, dass du dieses Profilfeld nicht nutzt',
+            'optionscode' => 'text',
+            'value' => '-1', // Default
+            'disporder' => 5
+        ),
+        'blacklist_player' => array(
+            'title' => 'Spieler Profilfeld',
+            'description' => 'Gib hier die ID von deinem Profilfeld ein, wo man den Spielernamen einträgt',
+            'optionscode' => 'text',
+            'value' => '-1', // Default
+            'disporder' => 6
+        ),
+        'blacklist_inplay' => array(
+            'title' => 'Inplaykategorie',
+            'description' => 'Gib hier die ID von der Inplaykategorie ein.',
+            'optionscode' => 'text',
+            'value' => '-1', // Default
+            'disporder' => 7
+        ),
+        'blacklist_archive' => array(
+            'title' => 'Archivkategorie',
+            'description' => 'Gib hier die ID von der Archivkategorie ein.',
+            'optionscode' => 'text',
+            'value' => '-1', // Default
+            'disporder' => 8
+        ),
+        'blacklist_echo' => array(
+            'title' => 'Rückmeldezeitraum',
+            'description' => 'Bis zu welchen Tag darf man sich zurückmelden? (Hinweis: bis zu diesem Tag wird auch der Hinweis auf dem Index angezeigt)',
+            'optionscode' => 'text',
+            'value' => '7', // Default
+            'disporder' => 9
+        ),
+    );
+
+    foreach($setting_array as $name => $setting){
+        $setting['name'] = $name;
+        $setting['gid'] = $gid;
+  
+        $db->insert_query('settings', $setting);
+    }
+
+    //Template blacklist bauen
+    $insert_array = array(
+        'title'		=> 'blacklist',
+        'template'	=> $db->escape_string('<html xml:lang="de" lang="de" xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <title>Blacklist</title>
+        {$headerinclude}
+        </head>
+        <body>
+        {$header}
+        <div class="panel" id="panel">
+        <div id="panel">$menu</div>
+            {$banner}
+            <h1>Blacklist vom 01.{$thisMonth}</h1>
+            
+        <table style="width:95%; margin:auto;">
+            <tr>
+                <td width="33%" class="thead">Blacklist</td>
+                <td width="33%" class="thead">Abwesend</td>
+            </tr>
+            <tr>
+                <td valign="top">{$userBlack}</td>
+                <td valign="top">{$away} </td>
+            </tr>
+        </table>
+            
+        </div>
+        {$footer}
+        </body>
+        </html>
+        <script>
+	   $(\'.delete\').click(function () {
+        if (confirm("Möchtest du diesen User streichen?")) {
+            $.post("blacklist.php", {
+                id: this.id,
+                action: \'delete\'
+            })
+            .done(function() {
+            location.reload(); 
+            })
+        }
+    });
+    </script>
+    <style>
+        .buttonBlack{
+            cursor: pointer;
+            padding-left: 3px;
+        }
+    </style>'),
+        'sid'		=> '-1',
+        'version'	=> '',
+        'dateline'	=> TIME_NOW
+      );
+      $db->insert_query("templates", $insert_array);
+
+      //Template blacklistIce bauen
+    $insert_array = array(
+        'title'		=> 'blacklistIce',
+        'template'	=> $db->escape_string('<html xml:lang="de" lang="de" xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <title>Blacklist</title>
+        {$headerinclude}
+        </head>
+        <body>
+        {$header}
+        <div class="panel" id="panel">
+        <div id="panel">$menu</div>
+            {$banner}
+            <h1>Blacklist vom 01.{$thisMonth}</h1>
+            
+        <table style="width:95%; margin:auto;">
+            <tr>
+                <td width="33%" class="thead">Blacklist</td>
+                <td width="33%" class="thead">Abwesend</td>
+                <td width="33%" class="thead">Auf Eis</td>
+            </tr>
+            <tr>
+                <td valign="top">{$userBlack}</td>
+                <td valign="top">{$away} </td>
+                <td valign="top">{$onIce} </td> 
+            </tr>
+        </table>
+            
+        </div>
+        {$footer}
+        </body>
+        </html>
+        <script>
+	   $(\'.delete\').click(function () {
+        if (confirm("Möchtest du diesen User streichen?")) {
+            $.post("blacklist.php", {
+                id: this.id,
+                action: \'delete\'
+            })
+            .done(function() {
+            location.reload(); 
+            })
+        }
+    });
+    </script>
+    <style>
+        .buttonBlack{
+            cursor: pointer;
+            padding-left: 3px;
+        }
+    </style>'),
+        'sid'		=> '-1',
+        'version'	=> '',
+        'dateline'	=> TIME_NOW
+      );
+      $db->insert_query("templates", $insert_array);
+
+      //Template blacklistUser bauen
+      $insert_array = array(
+        'title'		=> 'blacklistUser',
+        'template'	=> $db->escape_string('$username $deleteButton<br/>'),
+        'sid'		=> '-1',
+        'version'	=> '',
+        'dateline'	=> TIME_NOW
+      );
+      $db->insert_query("templates", $insert_array);
+
+      //Template blacklistUserGestrichen bauen
+      $insert_array = array(
+        'title'		=> 'blacklistUserAnnulled',
+        'template'	=> $db->escape_string('<s>$username</s><br/>'),
+        'sid'		=> '-1',
+        'version'	=> '',
+        'dateline'	=> TIME_NOW
+      );
+      $db->insert_query("templates", $insert_array);
+
+      //Template blacklistHeader bauen
+      $insert_array = array(
+        'title'		=> 'blacklistHeader',
+        'template'	=> $db->escape_string('<div class="pm_alert">Die aktuelle <a href="/blacklist.php">Blacklist</a> ist draußen. Von dir befinden sich <b>keine Charaktere</b> auf dieser. <a href="/blacklist.php?seen=1" title="Nicht mehr anzeigen"><span style="font-size: 14px;margin-top: -2px;float:right;">✕</span></a></div>'),
+        'sid'		=> '-1',
+        'version'	=> '',
+        'dateline'	=> TIME_NOW
+      );
+      $db->insert_query("templates", $insert_array);
+
+      //Template blacklistHeaderChara bauen
+      $insert_array = array(
+        'title'		=> 'blacklistHeaderChara',
+        'template'	=> $db->escape_string('<div class="pm_alert">Die aktuelle <a href="/blacklist.php">Blacklist</a> ist draußen. Von dir stehen auf der Blacklist: <b> {$charanames} </b> <a href="/blacklist.php?seen=1" title="Nicht mehr anzeigen"><span style="font-size: 14px;margin-top: -2px;float:right;">✕</span></a></div>'),
+        'sid'		=> '-1',
+        'version'	=> '',
+        'dateline'	=> TIME_NOW
+      );
+      $db->insert_query("templates", $insert_array);
+
+    rebuild_settings(); 
+}
+
+function blacklist_is_installed(){
+  global $db, $mybb;
+	if(isset($mybb->settings['blacklist_applicant'])) {
+			return true;
+	}
+	return false;
+}
+
+function blacklist_uninstall(){
+    global $db;
+    $db->delete_query('settings', "name IN('blacklist_guest','blacklist_applicant', 'blacklist_showUser', 'blacklist_teamaccs' 'blacklist_ice', 'blacklist_player', 'blacklist_inplay', 'blacklist_archive', 'blacklist_echo')");
+    $db->delete_query('settinggroups', "name = 'blacklist'");
+    $db->delete_query("templates", "title IN('blacklist','blacklistIce', 'blacklistUser', 'blacklistUserAnnulled', 'blacklistHeader', 'blacklistHeaderChara')");
+    $db->query("ALTER TABLE ".TABLE_PREFIX."users DROP isOnBlacklist");
+    $db->query("ALTER TABLE ".TABLE_PREFIX."users DROP isOnBlacklistAnnulled");
+    $db->query("ALTER TABLE ".TABLE_PREFIX."users DROP hasSeenBlacklist");
+    rebuild_settings();
+}
+
+function blacklist_activate(){
+  global $db, $mybb;
+  include MYBB_ROOT."/inc/adminfunctions_templates.php";
+  find_replace_templatesets("header", "#".preg_quote('{$awaitingusers}')."#i", '{$awaitingusers} {$header_blacklist}');
+}
+
+function blacklist_deactivate(){
+  global $db, $mybb;
+  include MYBB_ROOT."/inc/adminfunctions_templates.php";
+  find_replace_templatesets("header", "#".preg_quote('{$header_blacklist}')."#i", '', 0);
+}
+
+//Benachrichtung bei Blacklist
+$plugins->add_hook('global_intermediate', 'blacklist_alert');
+function blacklist_alert(){
+    global $db, $mybb, $templates, $header_blacklist; 
+
+    $alertDays = intval($mybb->settings['blacklist_echo']); 
+    $email = $mybb->user['email'];
+    
+    if(date("j", time()) == 1){
+        $db->query("UPDATE ".TABLE_PREFIX."users SET hasSeenBlacklist = 0 AND isOnBlacklist = 0 AND isOnBlacklistAnnulled = 0");
+    }
+
+    if($_GET['seen'] == 1){
+        $db->query("UPDATE ".TABLE_PREFIX."users SET hasSeenBlacklist = 1 WHERE email = '" . $email . "'");
+    }
+
+    $applicant = "";
+    if($mybb->settings['blacklist_applicant'] == "0"){
+        $applicant = "AND usergroup != 2";
+    }
+
+    $charas = $db->query("SELECT username,uid, hasSeenBlacklist
+    FROM ".TABLE_PREFIX."users 
+    WHERE email = '" . $email . "' AND isOnBlacklist = 1 AND isOnBlacklistAnnulled = 0 AND away = 0 " . $applicant ."
+    ORDER BY username");
+    $header_blacklist = "";
+    $charanames = "";
+    $dontSee = false;
+    $oneChara = true;
+    while($chara=$db->fetch_array($charas)) {
+        if($oneChara){
+            $charanames .=  $chara['username'];
+            $oneChara = false;
+        }else{
+            $charanames .=  ", " . $chara['username'];
+        }
+        if($chara['hasSeenBlacklist'] == 1){
+            $dontSee = true;
+        }
+    }
+    if(date("j", time()) <= $alertDays && $alertDays != -1 && $mybb->user['uid'] != 0 && !$dontSee){
+        if($charanames == ""){
+            eval("\$header_blacklist .= \"".$templates->get("blacklistHeader")."\";");
+        }else{
+            eval("\$header_blacklist .= \"".$templates->get("blacklistHeaderChara")."\";");
+        }
+    }
+}
+
+?>
